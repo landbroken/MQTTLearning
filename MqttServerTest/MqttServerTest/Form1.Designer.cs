a5c94a19076f6caa2b3b87c17d322eb8e898c2a4
landbroken <445299258@qq.com>
2018-05-09 14:52:07 +0800

    Initial commit

---
+++ MqttServerTest/MqttServerTest/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace MqttServerTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSubscribe = new System.Windows.Forms.Button();
+            this.txtSubTopic = new System.Windows.Forms.TextBox();
+            this.BtnPublish = new System.Windows.Forms.Button();
+            this.txtPubTopic = new System.Windows.Forms.TextBox();
+            this.txtSendMessage = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnSubscribe
+            // 
+            this.btnSubscribe.Location = new System.Drawing.Point(62, 57);
+            this.btnSubscribe.Name = "btnSubscribe";
+            this.btnSubscribe.Size = new System.Drawing.Size(122, 23);
+            this.btnSubscribe.TabIndex = 0;
+            this.btnSubscribe.Text = "btnSubscribe";
+            this.btnSubscribe.UseVisualStyleBackColor = true;
+            this.btnSubscribe.Click += new System.EventHandler(this.BtnSubscribe_ClickAsync);
+            // 
+            // txtSubTopic
+            // 
+            this.txtSubTopic.Location = new System.Drawing.Point(50, 140);
+            this.txtSubTopic.Multiline = true;
+            this.txtSubTopic.Name = "txtSubTopic";
+            this.txtSubTopic.Size = new System.Drawing.Size(341, 259);
+            this.txtSubTopic.TabIndex = 1;
+            // 
+            // BtnPublish
+            // 
+            this.BtnPublish.Location = new System.Drawing.Point(403, 72);
+            this.BtnPublish.Name = "BtnPublish";
+            this.BtnPublish.Size = new System.Drawing.Size(75, 23);
+            this.BtnPublish.TabIndex = 2;
+            this.BtnPublish.Text = "BtnPublish";
+            this.BtnPublish.UseVisualStyleBackColor = true;
+            this.BtnPublish.Click += new System.EventHandler(this.BtnPublish_Click);
+            // 
+            // txtPubTopic
+            // 
+            this.txtPubTopic.Location = new System.Drawing.Point(467, 106);
+            this.txtPubTopic.Multiline = true;
+            this.txtPubTopic.Name = "txtPubTopic";
+            this.txtPubTopic.Size = new System.Drawing.Size(341, 130);
+            this.txtPubTopic.TabIndex = 3;
+            // 
+            // txtSendMessage
+            // 
+            this.txtSendMessage.Location = new System.Drawing.Point(467, 269);
+            this.txtSendMessage.Multiline = true;
+            this.txtSendMessage.Name = "txtSendMessage";
+            this.txtSendMessage.Size = new System.Drawing.Size(341, 130);
+            this.txtSendMessage.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1275, 471);
+            this.Controls.Add(this.txtSendMessage);
+            this.Controls.Add(this.txtPubTopic);
+            this.Controls.Add(this.BtnPublish);
+            this.Controls.Add(this.txtSubTopic);
+            this.Controls.Add(this.btnSubscribe);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSubscribe;
+        private System.Windows.Forms.TextBox txtSubTopic;
+        private System.Windows.Forms.Button BtnPublish;
+        private System.Windows.Forms.TextBox txtPubTopic;
+        private System.Windows.Forms.TextBox txtSendMessage;
+    }
+}
+
---
+++ MqttServerTest/MqttServerTest/Form1.cs
@@ -0,0 +1,66 @@
+﻿using MQTTnet;
+using MQTTnet.Protocol;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace MqttServerTest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BtnPublish_Click(object sender, EventArgs e)
+        {
+            string topic = txtPubTopic.Text.Trim();
+
+            if (string.IsNullOrEmpty(topic))
+            {
+                MessageBox.Show("发布主题不能为空！");
+                return;
+            }
+
+            string inputString = txtSendMessage.Text.Trim();
+            var appMsg = new MqttApplicationMessage(topic, Encoding.UTF8.GetBytes(inputString), MqttQualityOfServiceLevel.AtMostOnce, false);
+            mqttClient.PublishAsync(appMsg);
+        }
+
+        private void BtnSubscribe_ClickAsync(object sender, EventArgs e)
+        {
+            string topic = txtSubTopic.Text.Trim();
+
+            if (string.IsNullOrEmpty(topic))
+            {
+                MessageBox.Show("订阅主题不能为空！");
+                return;
+            }
+
+            if (!mqttClient.IsConnected)
+            {
+                MessageBox.Show("MQTT客户端尚未连接！");
+                return;
+            }
+
+            mqttClient.SubscribeAsync(new List<TopicFilter> {
+                new TopicFilter(topic, MqttQualityOfServiceLevel.AtMostOnce)
+            });
+
+            txtReceiveMessage.AppendText($"已订阅[{topic}]主题" + Environment.NewLine);
+            txtSubTopic.Enabled = false;
+            btnSubscribe.Enabled = false;
+        }
+    }
+}
