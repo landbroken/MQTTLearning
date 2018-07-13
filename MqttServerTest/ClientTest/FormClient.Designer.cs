d57bbda794970de2e68629ae45d7532fd1af1dc6
landbroken <445299258@qq.com>
2018-07-13 16:27:41 +0800

    删掉了没用的东西

--- MqttServerTest/ClientTest/FormClient.Designer.cs
+++ MqttServerTest/ClientTest/FormClient.Designer.cs
@@ -44,13 +44,12 @@
             this.btnLogout = new System.Windows.Forms.Button();
             this.label5 = new System.Windows.Forms.Label();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.button1 = new System.Windows.Forms.Button();
-            this.txtClientId = new System.Windows.Forms.TextBox();
-            this.label6 = new System.Windows.Forms.Label();
-            this.txtPort = new System.Windows.Forms.TextBox();
-            this.label7 = new System.Windows.Forms.Label();
-            this.txtIp = new System.Windows.Forms.TextBox();
             this.label8 = new System.Windows.Forms.Label();
+            this.txtIp = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtPort = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtClientId = new System.Windows.Forms.TextBox();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -210,51 +209,14 @@
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "UserInfo";
             // 
-            // button1
+            // label8
             // 
-            this.button1.Location = new System.Drawing.Point(392, 412);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 16;
-            this.button1.Text = "test";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // txtClientId
-            // 
-            this.txtClientId.Location = new System.Drawing.Point(99, 20);
-            this.txtClientId.Multiline = true;
-            this.txtClientId.Name = "txtClientId";
-            this.txtClientId.Size = new System.Drawing.Size(115, 31);
-            this.txtClientId.TabIndex = 14;
-            this.txtClientId.Text = "client001";
-            // 
-            // label6
-            // 
-            this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(22, 29);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(71, 15);
-            this.label6.TabIndex = 15;
-            this.label6.Text = "clientId";
-            // 
-            // txtPort
-            // 
-            this.txtPort.Location = new System.Drawing.Point(284, 57);
-            this.txtPort.Multiline = true;
-            this.txtPort.Name = "txtPort";
-            this.txtPort.Size = new System.Drawing.Size(115, 31);
-            this.txtPort.TabIndex = 16;
-            this.txtPort.Text = "8222";
-            // 
-            // label7
-            // 
-            this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(220, 60);
-            this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(39, 15);
-            this.label7.TabIndex = 17;
-            this.label7.Text = "port";
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(220, 23);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(23, 15);
+            this.label8.TabIndex = 19;
+            this.label8.Text = "ip";
             // 
             // txtIp
             // 
@@ -265,21 +227,47 @@
             this.txtIp.TabIndex = 18;
             this.txtIp.Text = "192.168.88.3";
             // 
-            // label8
+            // label7
             // 
-            this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(220, 23);
-            this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(23, 15);
-            this.label8.TabIndex = 19;
-            this.label8.Text = "ip";
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(220, 60);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(39, 15);
+            this.label7.TabIndex = 17;
+            this.label7.Text = "port";
+            // 
+            // txtPort
+            // 
+            this.txtPort.Location = new System.Drawing.Point(284, 57);
+            this.txtPort.Multiline = true;
+            this.txtPort.Name = "txtPort";
+            this.txtPort.Size = new System.Drawing.Size(115, 31);
+            this.txtPort.TabIndex = 16;
+            this.txtPort.Text = "8222";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(22, 29);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(71, 15);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "clientId";
+            // 
+            // txtClientId
+            // 
+            this.txtClientId.Location = new System.Drawing.Point(99, 20);
+            this.txtClientId.Multiline = true;
+            this.txtClientId.Name = "txtClientId";
+            this.txtClientId.Size = new System.Drawing.Size(115, 31);
+            this.txtClientId.TabIndex = 14;
+            this.txtClientId.Text = "client001";
             // 
             // FormClient
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1275, 471);
-            this.Controls.Add(this.button1);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label2);
@@ -318,7 +306,6 @@
         private System.Windows.Forms.Button btnLogout;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.GroupBox groupBox1;
-        private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.TextBox txtClientId;
         private System.Windows.Forms.Label label7;
